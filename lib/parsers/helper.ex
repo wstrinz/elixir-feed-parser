91091f7b993fb22d6d3c42d0c0d2b83efb0671b3
Will Strinz <wstrinz@gmail.com>
2019-03-19 18:33:20 -0500

    trim date strings to prevent timex errors

--- lib/parsers/helper.ex
+++ lib/parsers/helper.ex
@@ -22,10 +22,10 @@ defmodule ElixirFeedParser.Parsers.Helper do
   def to_date_time(date_time_string, format) do
     case format do
       "ISO_8601" -> 
-        {:ok, date_time, _} = DateTime.from_iso8601(date_time_string)
+        {:ok, date_time, _} = DateTime.from_iso8601(date_time_string |> String.trim)
         date_time
       "RFC_1123" -> 
-        {:ok, date_time} = Timex.parse(date_time_string, "{RFC1123}")
+        {:ok, date_time} = Timex.parse(date_time_string |> String.trim, "{RFC1123}")
         date_time
     end
   end
